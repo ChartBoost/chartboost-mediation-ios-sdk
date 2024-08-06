128b5ee90b8a93ae86f77ac6f595c8dfbdeecc24
Haoxin Li <haoxin.li@chartboost.com>
2024-08-06 10:03:12 -0700

    remove obsolete files

--- HeliumSdk/HeliumSdk/Public/HeliumDelegates.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2018-2024 Chartboost, Inc.
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
-
-// Original Objective C implementation has been refactored in Swift.
-// Keep this header file so that existing import statements do not cause compiler errors.
--- HeliumSdk/HeliumSdkTests/ChartboostMediationSDKTests-Bridging-Header.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2018-2024 Chartboost, Inc.
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
-
-// This is file empty now, but we cannot remove this file yet until all Objective C code got removed.
